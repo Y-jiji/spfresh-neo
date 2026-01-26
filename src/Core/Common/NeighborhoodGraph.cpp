38fb944715c120ac855fcca466ae2522fdb7f643
Y-jiji <y.jijiji.data.science@gmail.com>
2026-01-26 11:10:38 -0500

    Refactor: remove KNNGraph

--- src/Core/Common/NeighborhoodGraph.cpp
+++ src/Core/Common/NeighborhoodGraph.cpp
@@ -2,15 +2,12 @@
 // Licensed under the MIT License.
 
 #include "Core/Common/NeighborhoodGraph.h"
-#include "Core/Common/KNearestNeighborhoodGraph.h"
 #include "Core/Common/RelativeNeighborhoodGraph.h"
 
 std::shared_ptr<SPTAG::COMMON::NeighborhoodGraph> SPTAG::COMMON::NeighborhoodGraph::CreateInstance(std::string type) {
     std::shared_ptr<SPTAG::COMMON::NeighborhoodGraph> res;
     if (type == "RNG") {
         res.reset(new SPTAG::COMMON::RelativeNeighborhoodGraph);
-    } else if (type == "NNG") {
-        res.reset(new SPTAG::COMMON::KNearestNeighborhoodGraph);
     }
     return res;
 }
