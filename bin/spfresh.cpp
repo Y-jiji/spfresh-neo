4db7a7155c81c5b31a9e460fc6b9cdaa2309661a
Y-jiji <y.jijiji.data.science@gmail.com>
2026-01-26 00:07:51 -0500

    Refactor: squash files that are only included once

--- bin/spfresh.cpp
+++ bin/spfresh.cpp
@@ -1,6 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <iostream>
+
+#include "Core/Common.h"
+#include "Core/VectorIndex.h"
+#include "Core/SPANN/Index.h"
+#include "Helper/SimpleIniReader.h"
+#include "Helper/VectorSetReader.h"
+#include "Helper/StringConvert.h"
+#include "Core/Common/TruthSet.h"
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include "Core/Common.h"
 #include "Core/Common/TruthSet.h"
 #include "Core/SPANN/Index.h"
@@ -1069,4 +1082,14 @@ int UpdateTest(const char* storePath) {
 
     return 0;
 }
+
 }  // namespace SPTAG::SSDServing::SPFresh
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        SPTAG::LOG(SPTAG::Helper::LogLevel::LL_Error, "spfresh storePath\n");
+        exit(-1);
+    }
+    auto ret = SPTAG::SSDServing::SPFresh::UpdateTest(argv[1]);
+    return ret;
+}
--- bin/ssdserving.cpp
+++ bin/ssdserving.cpp
@@ -1,16 +1,80 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#pragma once
-#include <limits>
+#include <iostream>
+#include <string>
+#include <map>
+#include <chrono>
 
 #include "Core/Common.h"
+#include "Core/VectorIndex.h"
+#include "Core/SPANN/Index.h"
+#include "Helper/SimpleIniReader.h"
+#include "Helper/VectorSetReader.h"
+#include "Core/Common/TruthSet.h"
 #include "Utils/DistanceUtils.h"
 #include "Core/Common/QueryResultSet.h"
 #include "Core/SPANN/Index.h"
 #include "Helper/VectorSetReader.h"
-#include "Helper/StringConvert.h"
-#include "SSDServing/Utils.h"
+
+namespace SPTAG::SSDServing {
+
+int BootProgram(std::map<std::string, std::map<std::string, std::string>>* config_map, const char* configurationPath = nullptr);
+
+const std::string SEC_BASE = "Base";
+const std::string SEC_SELECT_HEAD = "SelectHead";
+const std::string SEC_BUILD_HEAD = "BuildHead";
+const std::string SEC_BUILD_SSD_INDEX = "BuildSSDIndex";
+const std::string SEC_SEARCH_SSD_INDEX = "SearchSSDIndex";
+
+}  // namespace SPTAG::SSDServing
+
+
+namespace SPTAG::SSDServing::Utils {
+typedef std::chrono::steady_clock SteadClock;
+
+double getMsInterval(std::chrono::steady_clock::time_point start, std::chrono::steady_clock::time_point end) {
+    return (std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() * 1.0) / 1000.0;
+}
+
+double getSecInterval(std::chrono::steady_clock::time_point start, std::chrono::steady_clock::time_point end) {
+    return (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() * 1.0) / 1000.0;
+}
+
+double getMinInterval(std::chrono::steady_clock::time_point start, std::chrono::steady_clock::time_point end) {
+    return (std::chrono::duration_cast<std::chrono::seconds>(end - start).count() * 1.0) / 60.0;
+}
+
+/// Clock class
+class StopW {
+   private:
+    std::chrono::steady_clock::time_point time_begin;
+
+   public:
+    StopW() {
+        time_begin = std::chrono::steady_clock::now();
+    }
+
+    double getElapsedMs() {
+        std::chrono::steady_clock::time_point time_end = std::chrono::steady_clock::now();
+        return getMsInterval(time_begin, time_end);
+    }
+
+    double getElapsedSec() {
+        std::chrono::steady_clock::time_point time_end = std::chrono::steady_clock::now();
+        return getSecInterval(time_begin, time_end);
+    }
+
+    double getElapsedMin() {
+        std::chrono::steady_clock::time_point time_end = std::chrono::steady_clock::now();
+        return getMinInterval(time_begin, time_end);
+    }
+
+    void reset() {
+        time_begin = std::chrono::steady_clock::now();
+    }
+};
+}  // namespace SPTAG::SSDServing::Utils
 
 namespace SPTAG::SSDServing::SSDIndex {
 
@@ -464,3 +528,122 @@ void Search(SPANN::Index<ValueType>* p_index) {
 }
 
 }  // namespace SPTAG::SSDServing::SSDIndex
+
+namespace SPTAG::SSDServing {
+
+int BootProgram(std::map<std::string, std::map<std::string, std::string>>* config_map, const char* configurationPath) {
+    SPTAG::VectorValueType valueType = SPTAG::VectorValueType::Undefined;
+    SPTAG::DistCalcMethod distCalcMethod = SPTAG::DistCalcMethod::Undefined;
+
+    bool searchSSD = false;
+    Helper::IniReader iniReader;
+    iniReader.LoadIniFile(configurationPath);
+    (*config_map)[SEC_BASE] = iniReader.GetParameters(SEC_BASE);
+    (*config_map)[SEC_SELECT_HEAD] = iniReader.GetParameters(SEC_SELECT_HEAD);
+    (*config_map)[SEC_BUILD_HEAD] = iniReader.GetParameters(SEC_BUILD_HEAD);
+    (*config_map)[SEC_BUILD_SSD_INDEX] = iniReader.GetParameters(SEC_BUILD_SSD_INDEX);
+
+    valueType = iniReader.GetParameter(SEC_BASE, "ValueType", valueType);
+    distCalcMethod = iniReader.GetParameter(SEC_BASE, "DistCalcMethod", distCalcMethod);
+    bool buildSSD = iniReader.GetParameter(SEC_BUILD_SSD_INDEX, "isExecute", false);
+    searchSSD = iniReader.GetParameter(SEC_SEARCH_SSD_INDEX, "isExecute", false);
+
+    for (auto& KV : iniReader.GetParameters(SEC_SEARCH_SSD_INDEX)) {
+        std::string param = KV.first, value = KV.second;
+        if (buildSSD && Helper::StrUtils::StrEqualIgnoreCase(param.c_str(), "BuildSsdIndex"))
+            continue;
+        if (buildSSD && Helper::StrUtils::StrEqualIgnoreCase(param.c_str(), "isExecute"))
+            continue;
+        if (Helper::StrUtils::StrEqualIgnoreCase(param.c_str(), "PostingPageLimit"))
+            param = "SearchPostingPageLimit";
+        if (Helper::StrUtils::StrEqualIgnoreCase(param.c_str(), "InternalResultNum"))
+            param = "SearchInternalResultNum";
+        (*config_map)[SEC_BUILD_SSD_INDEX][param] = value;
+    }
+
+    std::shared_ptr<SPTAG::VectorIndex> index;
+    switch (valueType) {
+#define DefineVectorValueType(Name, Type)                                           \
+    case SPTAG::VectorValueType::Name:                                              \
+        index = std::shared_ptr<SPTAG::VectorIndex>(new SPTAG::SPANN::Index<Type>); \
+        break;
+
+#include "Core/DefinitionList.h"
+#undef DefineVectorValueType
+        default:
+            LOG(Helper::LogLevel::LL_Error, "Cannot create Index with ValueType %s!\n", (*config_map)[SEC_BASE]["ValueType"].c_str());
+            return -1;
+    }
+
+    for (auto& sectionKV : *config_map) {
+        for (auto& KV : sectionKV.second) {
+            index->SetParameter(KV.first, KV.second, sectionKV.first);
+        }
+    }
+
+    if (index->BuildIndex() != SPTAG::ErrorCode::Success) {
+        LOG(Helper::LogLevel::LL_Error, "Failed to build index.\n");
+        exit(1);
+    }
+
+    SPTAG::SPANN::Options* opts = nullptr;
+
+#define DefineVectorValueType(Name, Type)                               \
+    if (index->GetVectorValueType() == SPTAG::VectorValueType::Name) {  \
+        opts = ((SPTAG::SPANN::Index<Type>*)index.get())->GetOptions(); \
+    }
+
+#include "Core/DefinitionList.h"
+#undef DefineVectorValueType
+
+    if (opts == nullptr) {
+        LOG(Helper::LogLevel::LL_Error, "Cannot get options.\n");
+        exit(1);
+    }
+
+    if (opts->m_generateTruth) {
+        LOG(Helper::LogLevel::LL_Info, "Start generating truth. It's maybe a long time.\n");
+        SPTAG::SizeType dim = opts->m_dim;
+        std::shared_ptr<Helper::ReaderOptions> vectorOptions(new Helper::ReaderOptions(valueType, dim, opts->m_vectorDelimiter));
+        auto vectorReader = Helper::VectorSetReader::CreateInstance(vectorOptions);
+        if (SPTAG::ErrorCode::Success != vectorReader->LoadFile(opts->m_vectorPath)) {
+            LOG(Helper::LogLevel::LL_Error, "Failed to read vector file.\n");
+            exit(1);
+        }
+        std::shared_ptr<Helper::ReaderOptions> queryOptions(new Helper::ReaderOptions(opts->m_valueType, opts->m_dim, opts->m_queryDelimiter));
+        auto queryReader = Helper::VectorSetReader::CreateInstance(queryOptions);
+        if (SPTAG::ErrorCode::Success != queryReader->LoadFile(opts->m_queryPath)) {
+            LOG(Helper::LogLevel::LL_Error, "Failed to read query file.\n");
+            exit(1);
+        }
+        auto vectorSet = vectorReader->GetVectorSet();
+        auto querySet = queryReader->GetVectorSet();
+        if (distCalcMethod == SPTAG::DistCalcMethod::Cosine)
+            vectorSet->Normalize(opts->m_iSSDNumberOfThreads);
+    }
+
+    if (searchSSD) {
+#define DefineVectorValueType(Name, Type)                            \
+    if (opts->m_valueType == SPTAG::VectorValueType::Name) {         \
+        SSDIndex::Search((SPTAG::SPANN::Index<Type>*)(index.get())); \
+    }
+
+#include "Core/DefinitionList.h"
+#undef DefineVectorValueType
+    }
+    return 0;
+}
+}  // namespace SPTAG::SSDServing
+
+int main(int argc, char* argv[]) {
+    using SPTAG::g_pLogger;
+    if (argc < 2) {
+        LOG(SPTAG::Helper::LogLevel::LL_Error,
+            "ssdserving configFilePath\n");
+        exit(-1);
+    }
+
+    std::map<std::string, std::map<std::string, std::string>> my_map;
+    auto ret = SPTAG::SSDServing::BootProgram(&my_map, argv[1]);
+    return ret;
+}
